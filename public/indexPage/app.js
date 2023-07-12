365339c1ff07427bfb251f3b31f5929c3a5eb374
Jack Snyder <jacksnyde@gmail.com>
2023-07-12 12:58:18 -0600

    Index and user update

--- public/indexPage/app.js
+++ public/indexPage/app.js
@@ -1,10 +1,117 @@
+const URL = "http://localhost:8080/";
+
 Vue.createApp({
   data() {
     return {
       userSession: false,
       search: "",
+      showModal: false,
+      currentModal: "signin",
+      user: {
+        name: "",
+        email: "",
+        phoneNumber: "",
+        password: "",
+      },
+      loginUser: {
+        email: "",
+        password: "",
+      },
     };
   },
-  methods: {},
-  created: function () {},
+  methods: {
+    toggleModal: function () {
+      if (this.showModal) {
+        this.showModal = false;
+      } else {
+        this.showModal = true;
+      }
+    },
+    swapModal: function () {
+      this.name = "";
+      this.email = "";
+      this.phone = "";
+      this.password = "";
+      if (this.currentModal == "signin") {
+        this.currentModal = "signup";
+      } else {
+        this.currentModal = "signin";
+      }
+    },
+
+    // Sessions and users
+
+    signUp: function () {
+      var myHeaders = new Headers();
+      myHeaders.append("Content-Type", "application/json");
+
+      var options = {
+        method: "POST",
+        headers: myHeaders,
+        body: JSON.stringify(this.user),
+      };
+
+      fetch(URL + "users", options).then((response) => {
+        if (response.status == 201) {
+          this.createSession();
+        } else {
+          alert("Unable to create user.");
+        }
+      });
+    },
+    createSession: function () {
+      var myHeaders = new Headers();
+      myHeaders.append("Content-Type", "application/json");
+
+      var options = {
+        method: "POST",
+        headers: myHeaders,
+        body: JSON.stringify(this.loginUser),
+        credentials: "include",
+      };
+
+      fetch(URL + "session", options).then((response) => {
+        if (response.status == 201) {
+          response.text().then((data) => {
+            if (data) {
+              data = JSON.parse(data);
+              this.user.name = data.name;
+              this.userSession = true;
+              this.toggleModal();
+            } else {
+              alert("Can't log in.");
+            }
+          });
+        } else {
+          ("Unable to create user.");
+        }
+      });
+    },
+    loggedIn: function () {
+      var options = {
+        credentials: "include",
+      };
+
+      fetch(URL + "session", options)
+        .then((response) => response.json())
+        .then((data) => {
+          if (data) {
+            console.log(data && data.cookie && data.userId);
+            this.userSession = true;
+          }
+        });
+    },
+    signOut: function () {
+      var options = {
+        method: "DELETE",
+        credentials: "include",
+      };
+      fetch(URL + "session", options).then((response) => {
+        this.user.name = "";
+      });
+    },
+  },
+  created: function () {
+    this.loggedIn();
+  },
 }).mount("#app");
