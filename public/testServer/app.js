154f6ef654c4b91614d70db937fbf12fe181dce1
root <root@DESKTOP-Q8JBBF4>
2023-07-13 12:32:31 -0600

    changes

---
+++ public/testServer/app.js
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+import axios from 'axios'
+
+import './App.css'
+
+async function postImage({image, description}) {
+  const formData = new FormData();
+  formData.append("image", image)
+  formData.append("description", description)
+
+  const result = await axios.post('/images', formData, { headers: {'Content-Type': 'multipart/form-data'}})
+  return result.data
+}
+
+
+function App() {
+
+  const [file, setFile] = useState()
+  const [description, setDescription] = useState("")
+  const [images, setImages] = useState([])
+
+  const submit = async event => {
+    event.preventDefault()
+    const result = await postImage({image: file, description})
+    setImages([result.image, ...images])
+  }
+
+  const fileSelected = event => {
+    const file = event.target.files[0]
+    setFile(file)
+  }
+
+  return (
+    <div className="App">
+      <form onSubmit={submit}>
+        <input onChange={fileSelected} type="file" accept="image/*"></input>
+        <input value={description} onChange={e => setDescription(e.target.value)} type="text"></input>
+        <button type="submit">Submit</button>
+      </form>
+
+      { images.map( image => (
+        <div key={image}>
+          <img src={image}></img>
+        </div>
+      ))}
+
+    </div>
+  );
+}
+
+export default App;
