537b5cf1960e430c80d81e59d98f38064db7692b
MacKay Roy <mackay.roy1@gmail.com>
2023-07-18 12:47:25 -0600

    Updated

--- public/createListing/createListing.js
+++ public/createListing/createListing.js
@@ -17,6 +17,7 @@ Vue.createApp({
         parking: false,
         amenities: [],
         description: "",
+        photos: "",
       },
     };
   },
@@ -102,6 +103,26 @@ Vue.createApp({
 
     addAmenity: function () {
       this.propertyInfo.amenities.push({ text: "" });
+    },
+    async postImage() {
+      const formData = new FormData();
+      formData.append("file", this.$refs.uploadImage.files[0]);
+      try {
+        const response = await fetch("http://localhost:8080/images", {
+          method: "POST",
+          body: formData,
+        });
+
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+
+        const result = await response.json();
+        return result;
+      } catch (error) {
+        console.error(error);
+        throw error;
+      }
     },
   },
   created: function () {},
